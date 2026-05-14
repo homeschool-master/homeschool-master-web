4bcbdd66226e137f8dbcf9b9d004bddcc0081208
Robert Masters <robertmasters2478@gmail.com>
2026-05-14 13:16:39 -0400

    bring footer up to each page so it always displays

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,40 @@
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
+import { LANDING_CONTENT } from '../../constants/constants'
+
+const Footer = () => {
+  const { footer } = LANDING_CONTENT
+
+  return (
+    <footer className='landing__footer'>
+      <div className='landing__footer-brand'>
+        <Link to='/' className='landing__footer-logo'>
+          <img src={logo} alt='Homeschool Master' style={{ height: '24px', width: 'auto' }} />
+          <span>HOMESCHOOL</span>
+          <span className='landing__footer-logo-master'>MASTER</span>
+        </Link>
+        <p className='landing__footer-tagline'>{footer.tagline}</p>
+        <div className='landing__footer-socials'>
+          {footer.socials.map((social) => (
+            <a key={social} href='#' className='landing__footer-social' aria-label={social}>
+              {social[0].toUpperCase()}
+            </a>
+          ))}
+        </div>
+      </div>
+      {footer.columns.map((col) => (
+        <div key={col.heading} className='landing__footer-column'>
+          <h4 className='landing__footer-column-heading'>{col.heading}</h4>
+          <ul>
+            {col.links.map((link) => (
+              <li key={link}><a href='#'>{link}</a></li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <p className='landing__footer-copyright'>{footer.copyright}</p>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/marketing/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer>
-      <p>Homeschool Master</p>
-    </footer>
-  )
-}
-
-export default Footer
