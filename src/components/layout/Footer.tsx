3ddd42fc782f65d7a0271c5d898a8e753a84b9ba
Robert Masters <robertmasters2478@gmail.com>
2026-05-15 15:42:31 -0400

    update footer designs.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,34 +6,60 @@ const Footer = () => {
   const { footer } = LANDING_CONTENT
 
   return (
-    <footer className='landing__footer'>
-      <div className='landing__footer-inner'>
-        <div className='landing__footer-brand'>
-          <Link to='/' className='landing__footer-logo'>
-            <img src={logo} alt='Homeschool Master' style={{ height: '24px', width: 'auto' }} />
-            <span>HOMESCHOOL</span>
-            <span className='landing__footer-logo-master'>MASTER</span>
-          </Link>
-          <p className='landing__footer-tagline'>{footer.tagline}</p>
-          <div className='landing__footer-socials'>
-            {footer.socials.map((social) => (
-              <a key={social} href='#' className='landing__footer-social' aria-label={social}>
-                {social[0].toUpperCase()}
-              </a>
+    <footer className='footer'>
+      <div className='footer__main'>
+        <div className='footer__inner'>
+          <div className='footer__brand'>
+            <Link to='/' className='footer__logo'>
+              <img src={logo} alt='Homeschool Master' />
+              <span className='footer__logo-homeschool'>HOMESCHOOL</span>
+              <span className='footer__logo-master'>MASTER</span>
+            </Link>
+            <p className='footer__tagline'>{footer.tagline}</p>
+            <div className='footer__socials'>
+              {footer.socials.map((social) => (
+                <a key={social} href='#' className='footer__social' aria-label={social}>
+                  {social[0].toUpperCase()}
+                </a>
+              ))}
+            </div>
+          </div>
+          {footer.columns.map((col) => (
+            <div key={col.heading} className='footer__column'>
+              <h4 className='footer__column-heading'>{col.heading}</h4>
+              <ul>
+                {col.links.map((link) => (
+                  <li key={link}><a href='#'>{link}</a></li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div className='footer__newsletter'>
+            <h4 className='footer__column-heading'>{footer.newsletter.heading}</h4>
+            <p className='footer__newsletter-subtext'>{footer.newsletter.subtext}</p>
+            <div className='footer__newsletter-form'>
+              <input
+                type='email'
+                placeholder={footer.newsletter.placeholder}
+                className='footer__newsletter-input'
+              />
+              <button className='footer__newsletter-btn'>{footer.newsletter.buttonText}</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='footer__bottom'>
+        <div className='footer__bottom-inner'>
+          <p className='footer__copyright'>{footer.copyright}</p>
+          <div className='footer__legal'>
+            {footer.legalLinks.map((link, index) => (
+              <span key={link}>
+                <a href='#'>{link}</a>
+                {index < footer.legalLinks.length - 1 && <span className='footer__legal-dot'> · </span>}
+              </span>
             ))}
           </div>
         </div>
-        {footer.columns.map((col) => (
-          <div key={col.heading} className='landing__footer-column'>
-            <h4 className='landing__footer-column-heading'>{col.heading}</h4>
-            <ul>
-              {col.links.map((link) => (
-                <li key={link}><a href='#'>{link}</a></li>
-              ))}
-            </ul>
-          </div>
-        ))}
-        <p className='landing__footer-copyright'>{footer.copyright}</p>
       </div>
     </footer>
   )
