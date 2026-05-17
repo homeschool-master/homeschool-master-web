3e04e8efd17eb7962eb4281aee830e77ad722f2b
Robert Masters <robertmasters2478@gmail.com>
2026-05-17 00:32:10 -0400

    add null check for user.

--- src/pages/app/DashboardPage.tsx
+++ src/pages/app/DashboardPage.tsx
@@ -2,17 +2,19 @@ import { useSelector } from "react-redux";
 import type { RootState } from "../../store";
 
 const DashboardPage = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
 
-const user = useSelector((state: RootState) => state.auth.user)
-const firstName = user.firstName.charAt(0).toUpperCase() + user?.firstName?.slice(1)
-const lastName = user.lastName.charAt(0).toUpperCase() + user?.lastName?.slice(1)
+  if (!user) return null;
 
-return (
+  const firstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+  const lastName = user.lastName.charAt(0).toUpperCase() + user.lastName.slice(1);
+
+  return (
     <div>
       <h1>DashboardPage</h1>
       <div>Welcome {`${firstName} ${lastName}`}</div>
     </div>
-  )
+  );
 };
 
 export default DashboardPage;
